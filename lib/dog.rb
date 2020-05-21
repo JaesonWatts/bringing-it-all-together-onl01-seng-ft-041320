28a73cd287628869f60834cd38141707385822e9
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-21 06:10:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,6 +65,21 @@ class Dog
     new_from_db(result[0])
   end
   
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+    SQL
+
+    dog = DB[:conn].execute(sql, name, breed).first
+
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+    new_dog
+  end
   
 end
 
